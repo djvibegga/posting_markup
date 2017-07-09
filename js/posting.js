46bb1b63aeaa19ffc485c3473d4621753d7fda6a
sevs <s_evs@bk.ru>
2017-07-09 10:23:23 +0300

    random img

--- js/posting.js
+++ js/posting.js
@@ -10,3 +10,25 @@ $(document).on('click', '.panel-heading span.clickable', function(e){
 		$this.find('i').removeClass('fa fa-chevron-down').addClass('fa fa-chevron-up');
 	}
 })
+$(function(){
+
+    $('.spinner .btn:first-of-type').on('click', function() {
+      var btn = $(this);
+      var input = btn.closest('.spinner').find('input');
+      if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {    
+        input.val(parseInt(input.val(), 10) + 1);
+      } else {
+        btn.next("disabled", true);
+      }
+    });
+    $('.spinner .btn:last-of-type').on('click', function() {
+      var btn = $(this);
+      var input = btn.closest('.spinner').find('input');
+      if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {    
+        input.val(parseInt(input.val(), 10) - 1);
+      } else {
+        btn.prev("disabled", true);
+      }
+    });
+
+})
